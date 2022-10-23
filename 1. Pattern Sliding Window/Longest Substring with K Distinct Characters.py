96a9848b24ef88c4b7e3c5ffc231b984ac454919
gl-24 <gl.ece43@gmail.com>
2022-10-23 13:11:53 +0530

    Alternative solution

--- 1. Pattern Sliding Window/Longest Substring with K Distinct Characters.py
+++ 1. Pattern Sliding Window/Longest Substring with K Distinct Characters.py
@@ -14,24 +14,45 @@ Input: String="cbbebi", K=3
 Output: 5
 Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
 '''
+# def longest_substring_with_distinct_characters(s,k):
+#     start = end = 0
+#     d = 0
+#     mp = dict()
+#     while end < len(s):
+#         if s[end] not in mp:
+#             mp[s[end]] = 1 
+#         else:
+#             mp[s[end]] += 1
+        
+#         while len(mp) > k :
+#             mp[s[start]] = mp.get(s[start],0) - 1
+#             if mp[s[start]] == 0:
+#                 mp.pop(s[start])
+#             start += 1
+#         d = max(d,end - start + 1)
+#         end += 1
+#     return d
+
+
 def longest_substring_with_distinct_characters(s,k):
     start = end = 0
     d = 0
+    counter = 0
     mp = dict()
     while end < len(s):
-        if s[end] not in mp:
+        if mp.get(s[end], 0) == 0:
+            counter += 1
             mp[s[end]] = 1 
-        else:
-            mp[s[end]] += 1
-        
-        while len(mp) > k :
-            mp[s[start]] = mp.get(s[start],0) - 1
-            if mp[s[start]] == 0:
-                mp.pop(s[start])
-            start += 1
-        d = max(d,end - start + 1)
+
+        while counter > k :
+            if mp.get(s[start] ,0 ) == 1:
+                mp[s[start]] -= 1 
+                counter -= 1 
+                start += 1
+        d = max(d,end-start + 1)
         end += 1
     return d
+
 
 def main():
     s = "cbbebi"
@@ -41,6 +62,7 @@ def main():
 
 if __name__ == "__main__":
     main()
-
+'''
 Time Complexity : O(N)
 Space Complexity : O(1)
+'''
