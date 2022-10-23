85e28857af8f55add91ea3a84cc2cd031eb9e7fd
gl-24 <gl.ece43@gmail.com>
2022-10-23 11:27:27 +0530

    :sparkles:

--- 1. Pattern Sliding Window/Longest Substring with K Distinct Characters.py
+++ 1. Pattern Sliding Window/Longest Substring with K Distinct Characters.py
@@ -14,3 +14,30 @@ Input: String="cbbebi", K=3
 Output: 5
 Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
 '''
+def longest_substring_with_distinct_characters(s,k):
+    start = end = 0
+    d = 0
+    mp = dict()
+    while end < len(s):
+        if s[end] not in mp:
+            mp[s[end]] = 1 
+        else:
+            mp[s[end]] += 1
+        
+        while len(mp) > k :
+            mp[s[start]] = mp.get(s[start],0) - 1
+            if mp[s[start]] == 0:
+                mp.pop(s[start])
+            start += 1
+        d = max(d,end - start + 1)
+        end += 1
+    return d     
+
+def main():
+    s = "cbbebi"
+    k = 3
+    ans = longest_substring_with_distinct_characters(s,k)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
