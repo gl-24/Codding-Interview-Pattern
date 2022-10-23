c2664a0cfd25ed54d82d54db9541cab46f27852b
gl-24 <gl.ece43@gmail.com>
2022-10-23 15:19:21 +0530

    :bulb:

--- 1. Pattern Sliding Window/No repeat Substring.py
+++ 1. Pattern Sliding Window/No repeat Substring.py
@@ -16,4 +16,24 @@ Explanation: Longest substrings without any repeating characters are "abc" & "cd
 '''
 
 def non_repeat_substring(s:str):
-    
+    start = end = 0
+    d = 0 
+    mp = {}
+    while end < len(s):
+        ch = s[end] 
+        mp[ch] = mp.get(ch,0) + 1 
+        while mp[ch] > 1:
+            mp[s[start]] -= 1
+            start += 1
+        d = max(d, end - start + 1)
+        end += 1
+    return d 
+
+def main():
+    s = "aabccbb"
+    ans = non_repeat_substring(s)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
+
