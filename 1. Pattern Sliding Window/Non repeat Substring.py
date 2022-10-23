0db3fd851f416007c3b24bb78419dd4fad9ad862
gl-24 <gl.ece43@gmail.com>
2022-10-23 15:21:31 +0530

    Comments Added :bulb:

---
+++ 1. Pattern Sliding Window/Non repeat Substring.py
@@ -0,0 +1,43 @@
+'''
+Problem Statement 
+Given a string, find the length of the longest substring which has no repeating characters.
+Example 1:
+Input: String="aabccbb"
+Output: 3
+Explanation: The longest substring without any repeating characters is "abc".
+Example 2:
+Input: String="abbbb"
+Output: 2
+Explanation: The longest substring without any repeating characters is "ab".
+Example 3:
+Input: String="abccde"
+Output: 3
+Explanation: Longest substrings without any repeating characters are "abc" & "cde".
+'''
+
+def non_repeat_substring(s:str):
+    start = end = 0
+    d = 0 
+    mp = {}
+    while end < len(s):
+        ch = s[end] 
+        mp[ch] = mp.get(ch,0) + 1 
+        while mp[ch] > 1:
+            mp[s[start]] -= 1
+            start += 1
+        d = max(d, end - start + 1)
+        end += 1
+    return d 
+
+def main():
+    s = "aabccbb"
+    ans = non_repeat_substring(s)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
+
+'''
+Time Complexity : O(N)
+Space Complexity : O(N)
+'''
