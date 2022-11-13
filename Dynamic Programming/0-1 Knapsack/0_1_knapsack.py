5f4003b4616d40fdb03a8a4424e5750313d0d998
gl-24 <gl.ece43@gmail.com>
2022-11-14 00:34:47 +0530

    if and elif

--- Dynamic Programming/0-1 Knapsack/0_1_knapsack.py
+++ Dynamic Programming/0-1 Knapsack/0_1_knapsack.py
@@ -52,7 +52,7 @@ def knapsack(wt,val,W,n):
             if i == 0 or j == 0:
                 t[i][j] = 0
 
-            if wt[i-1] <= j:
+            elif wt[i-1] <= j:
                 t[i][j] = max(val[i-1] + t[i-1][j - wt[i-1]], t[i-1][j])
 
             else:
---
+++ Dynamic Programming/0-1 Knapsack/target_sum.py
@@ -0,0 +1,6 @@
+def target_sum(arr,n,sm):
+
+def main():
+
+if __name__ == "__main__":
+    main()
